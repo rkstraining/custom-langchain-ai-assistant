3b5f91bd9bef316834def03a6b2b36b0ea3c8f0d
RKS <rks.training93@gmail.com>
2025-09-19 19:13:53 +0530

    Sec 25 Agent re-built using langchain module.

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
@@ -22,6 +25,13 @@ llm = ChatGoogleGenerativeAI(
     temperature=0.5
 )
 
+prompt = ChatPromptTemplate.from_messages([
+    ("system", system_prompt),
+    (MessagesPlaceholder(variable_name="history")),
+    ("user", "{input}")]
+)
+
+chain = prompt | llm | StrOutputParser()
 
 print("Hi, I am Albert, how can I help you?")
 
@@ -30,7 +40,7 @@ while True:
     user_input = input("You: ")
     if user_input == "exit":
         break
-    history.append({"role": "user", "content": user_input})
-    response = llm.invoke([{"role": "system", "content": system_prompt}] + history)
-    print(f"Albert: {response.content}")
-    history.append({"role":"assistant", "content":response.content})
+    response = chain.invoke({"input": user_input, "history": history})
+    print(f"Albert: {response}")
+    history.append(HumanMessage(content=user_input))
+    history.append(AIMessage(content=response))
